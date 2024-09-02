c2b1557589835a3bfe8399a09c8e03be986363db
AtayDev <j.anadi@hotmail.com>
2024-09-02 16:23:33 +0200

    -Add Dockerfile to give instructions on how to build the app -Add CORS to the class WebConfig

--- canopee/src/main/java/com/jabran/canopee/config/WebConfig.java
+++ canopee/src/main/java/com/jabran/canopee/config/WebConfig.java
@@ -2,15 +2,35 @@ package com.jabran.canopee.config;
 
 import com.jabran.canopee.converters.HeadManagerConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private HeadManagerConverter headManagerConverter;
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(headManagerConverter);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontendUrl) // Netlify URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
